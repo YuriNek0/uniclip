e70a3315bdc5fd1027188f80eb9a19a9ce65b738
Sayuri Nekomiya <BBH@awsl.rip>
2026-01-01 18:39:04 +1100

    Updated flake to remove redundant dependency and moved deprecated system attribute.

--- flake.nix
+++ flake.nix
@@ -3,23 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = nixpkgs.legacyPackages.${system};
-  in rec {
-    packages.uniclip = pkgs.buildGoModule {
-      name = "uniclip";
-      src = ./.;
-      vendorHash = "sha256-ugrWrB0YVs/oWAR3TC3bEpt1VXQC1c3oLrvFJxlR8pw=";
-      meta = with pkgs.lib; {
-        description = "Cross-platform shared clipboard";
-        homepage = "https://github.com/quackduck/uniclip";
-        license = licenses.mit;
-        platforms = platforms.linux ++ platforms.darwin;
-      };
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs allSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          uniclip = pkgs.buildGoModule {
+            name = "uniclip";
+            src = ./.;
+            vendorHash = "sha256-ugrWrB0YVs/oWAR3TC3bEpt1VXQC1c3oLrvFJxlR8pw=";
+            
+            meta = with pkgs.lib; {
+              description = "Cross-platform shared clipboard";
+              homepage = "https://github.com/quackduck/uniclip";
+              license = licenses.mit;
+              platforms = [ pkgs.stdenv.hostPlatform.system ];
+              mainProgram = "uniclip";
+            };
+          };
+
+          default = self.packages.${system}.uniclip;
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.default);
     };
-    defaultPackage = packages.uniclip;
-  });
 }
